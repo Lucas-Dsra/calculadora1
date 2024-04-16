b8bd2e1d6a2c75518688b6e65febbb39db74c9e5
Lucas <lucas.dsra@gmail.com>
2024-04-16 20:41:58 -0300

    Inicio estilização

--- src/components/button/syles.js
+++ src/components/button/syles.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 export const ButtonContainer = styled.button`
     padding: 20px;
     border : 1px solid #cdcdcd;
-    background-color: #00aaff;
+    background-color: #3E3E3E;
     font-size: 24px;
     font-weight: 700;
     flex: 1;
